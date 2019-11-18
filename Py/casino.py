457c644751a78854e6e2f8252d3eeb964626af89
JackOWhiskey <jmarboeufpro@gmail.com>
2019-11-18 19:38:41 +0100

    ajout casino

---
+++ Py/casino.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+
+
+
+
+import os
+import random
+import math
+
+
+
+
+
+money = input("Saissisez le montant de départ: ")
+try:
+    money >=1000
+except
+
+randNum = random.randrange(40)
+math.ceil
