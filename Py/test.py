0d7c7efef4a810e50829a0e3b8ce4a06e697d828
Jack <56889351+Jack-CodeRepo@users.noreply.github.com>
2019-11-21 14:08:16 +0100

    ajout fonctions

--- Py/test.py
+++ Py/test.py
@@ -1,14 +1,32 @@
 # -*- coding: utf-8 -*-
 
+def addition():
+    a = input("saisissez le nombre de la table:")
+        = input("saisissez le nombre multiplcateur maximum:")
+    c = 0
+    b = b+1
+    while b != c:
+        print a, "+ ", c, "= ", a + c
+        c = c+1
+
 def multiplication():
+    a = input("saisissez le nombre nombre de la table:")
+    b = input("saisissez le nombre maximum multiplicateur:")
+    c = 0
 
-    nbTab = input("Saissisez le nombre de la table: ")
-    nbMax = input("Saissisez le nombre maximum à multiplier: ")
-    cnt = 0
-    while nbMax != cnt:
-        print(nbTab, "x", cnt, " = ", nbTab * cnt)
-        cnt = cnt + 1
-        break
+    b = b+1
+    while b != c:
+        print a, "x ", c, "= ", a * c
+        c = c+1
 
-multiplication()
 
+
+appel = 0
+while appel != "lol":
+    appel = input("Quelle fonction appeller (1_addition 2_multiplication 3_arreter):")
+    if appel == "addition" or appel == 1:
+        addition()
+    elif appel == "multiplication"  or  appel == 2:
+        multiplication()
+    elif appel == "arreter"  or  appel == 3:
+        exit()
