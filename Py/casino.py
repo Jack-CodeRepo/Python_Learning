99785e76a7d173be21f38fab91492127b65c3fac
JackOWhiskey <jmarboeufpro@gmail.com>
2019-11-21 18:09:57 +0100

    amélioration lisibilité du code

--- Py/casino.py
+++ Py/casino.py
@@ -1,44 +1,19 @@
 # -*- coding: utf-8 -*-
 
+
+# ===================================================================================================
+#   INPORT
+# ===================================================================================================
+
 import os
 import random
 import math
 import time
 
-mise = -1
 
-def Pick_Money():
-    money = -1
-    print("Vous décidez de sortir et de prendre de l'argent.")
-    while money <= 0 or money > 100 or money == 777:
-        print("============================================================")
-        money = input("Saissisez un montant de départ compris entre 1 et 100:: ")
-        print("=>")
-        try:
-            money = int(money)
-        except ValueError:
-            print("Saisissez un chiffre.")
-            money = -1
-        if money == 666:
-            print("C'est le nombre d'un homme. Relisez l'Apocalypse de Saint Jean.")
-            money = -1
-
-        if money == 777:
-            print("Ca porte bonheur. Vous pouvez prendre ce montant. Bonne chance.")
-            print("Vous partez de chez vous avec ", money, " Brouzoufes.")
-            casino()
-
-    print("Vous partez de chez vous avec ", money, " Brouzoufes.")
-    casino()
-
-def Go_Sleep():
-    time.sleep(2)
-    print("Vous allez dormir.")
-    time.sleep(5)
-    print("Bonne Nuitée !")
-    time.sleep(5)
-    print("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ")
-
+# ===================================================================================================
+#   Declarations des fonctions
+# ===================================================================================================
 
 def Go_Out():
     print("Vous vous ennuyez.")
@@ -65,20 +40,62 @@ def Go_Out():
         if activity == 4:
             casino()
 
+
+
+def Go_Sleep():
+    time.sleep(2)
+    print("Vous allez dormir.")
+    time.sleep(5)
+    print("Bonne Nuitée !")
+    time.sleep(5)
+    print("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ")
+
+
+
+def Pick_Money():
+    money = -1
+    print("Vous décidez de sortir et de prendre de l'argent.")
+
+    while money <= 0 or money > 100:
+        print("============================================================")
+        money = input("Saissisez un montant de départ compris entre 1 et 100:: ")
+        print("=>")
+        try:
+            money = int(money)
+        except ValueError:
+            print("Saisissez un chiffre.")
+            money = -1
+        if money == 666:
+            print("C'est le nombre d'un homme. Relisez l'Apocalypse de Saint Jean.")
+            money = -1
+
+        if money == 777:
+            print("Ca porte bonheur. Vous pouvez prendre ce montant. Bonne chance.")
+            break
+    print("Vous partez de chez vous avec ", money, " Brouzoufes.")
+
+
+
 def Go_Bar():
     Pick_Money()
+
+
 
 def Go_Whores():
     Pick_Money()
 
+
+
 def casino():
     Pick_Money()
+    time.sleep(3)
     print("Vous voilà au casino.")
-    time.sleep(2)
+    time.sleep(3)
     print("RECTIFICATION: vous voilà dans un casino où il n'y a que des roulettes !")
 
 
 
-
-
+# ===================================================================================================
+#   Appel des fonctions
+# ===================================================================================================
 Go_Out()
