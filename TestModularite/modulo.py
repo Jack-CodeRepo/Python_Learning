0681f0ba98093a86aa57737a1404c6342e38ca5d
JMF <jacques.marboeuf@altran.com>
2019-11-22 15:30:17 +0100

    modularite semble ok

--- TestModularite/modulo.py
+++ TestModularite/modulo.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from test import *
+# from test import *
+
+from testPckg import test
+
 
 appel = -1
 
 while appel == -1:
     appel = input("Quelle fonction appeller (1_addition 2_multiplication 3_arreter):")
     if appel == "addition" or appel == 1:
-        addition()
+        test.addition()
     elif appel == "multiplication"  or  appel == 2:
-        multiplication()
+        test.multiplication()
     elif appel == "arreter"  or  appel == 3:
         exit()
