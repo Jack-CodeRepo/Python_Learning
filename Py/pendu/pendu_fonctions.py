1a96aaaa459d644b21621ba200f306490be03f60
JMF <jacques.marboeuf@altran.com>
2020-02-21 11:21:07 +0100

    [fix] thank you Mr Thieuw

--- Py/pendu/pendu_fonctions.py
+++ Py/pendu/pendu_fonctions.py
@@ -30,22 +30,15 @@ def joueurExist(playerName,fichier):
     f = open(fichier, "r")
     lines = f.readlines()
     f.close()
-    nbPlayer = len(lines)
     for line in lines:
-        nbPlayer = nbPlayer - 1
-        print("nbJoueur =>  ", nbPlayer)
         if playerName in line:
-            print("VICTOIRE")
             score = line.split(".")[1].strip()
             print("Le joueur", playerName, "a été trouvé et a un score de ", score, " points")
-            break
-        elif nbPlayer != 0:
-            continue
-        else:
-            f = open("score.txt", "a")
-            f.write(playerName + ".0\n")
-            print("Le joueur ", playerName," a ete ajoute au fichier de sauvegarde")
-            f.close()
+            return score
+    f = open("score.txt", "a")
+    f.write(playerName + ".0\n")
+    print("Le joueur ", playerName," a ete ajoute au fichier de sauvegarde")
+    f.close()
 
 
 
