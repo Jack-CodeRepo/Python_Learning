201c68b3478daa75dcd2fd41b62de12d2921ffc5
JackOWhiskey <jmarboeufpro@gmail.com>
2020-02-22 22:18:28 +0100

    bidouillage import

--- tentative POO/f01.py
+++ tentative POO/f01.py
@@ -1,14 +1,15 @@
 
-from model.cls01 import teacher
+# from model.cls01 import teacher
 
+from model import cls01
 
 if __name__ in __name__ == "__main__":
-    prof01 = teacher("StreetCred", "Bobby", 10, "ambigu")
+    prof01 = cls01.teacher("StreetCred", "Bobby", 10, "ambigu")
     print("premier professeur créé (attention il est armé)")
     print("nom: ", prof01.get_name(),"/ prenom: ", prof01.get_lastName(), "/ craie: ", prof01.get_craie(), "/ sexe: ", prof01.get_sexe())
     print()
     print("second professeur créé (attention il est naze, il n'a même pas de craie)")
-    prof02 = teacher("Pondbeef","Jack", 0, "Dame")
+    prof02 = cls01.teacher("Pondbeef","Jack", 0, "Dame")
     print("nom: ", prof02.get_name(), "/ prenom: ", prof02.get_lastName(), "/ craie: ", prof02.get_craie(), "/ sexe: ", prof02.get_sexe())
     print(prof02.get_lastName()," s'exclame \"VIE DE MERDE\"")
 
