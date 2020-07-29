31e1e4a11ebcf084f6e9f223b9392ad9fc16a7d6
JackOWhiskey <jmarboeufpro@gmail.com>
2020-07-29 21:00:37 +0200

    remplacement d fichier test.py => guiPoo.py

--- ngu_idle_calculator/pp_calculator_poo/guiPoo.py
+++ ngu_idle_calculator/pp_calculator_poo/guiPoo.py
@@ -29,158 +29,157 @@ def test_float(value):
     try:
         float(value)
     except ValueError:
-        print("please input integer number (no float).")
+        print("please input float number.")
         return test
     return float(value)
+
+
 
 # ==================================================================================================
 #   CLASSES
 # ==================================================================================================
 
-class pp_calculator:
-
-    # ---------- construtor DEBUT ----------
-    def __init__(self, parent, x, y):
+class interface(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self)
         self.parent = parent
-        
-        # init // variables utilisées par le calcul
-        self.goal = ""
-        self.time = ""
-        self.point = ""
         self.time_one_pp = ""
         self.time_goal = ""
+        self.output = ""
 
-        # init // variables contenant les messages d'output
-        self.string01= ''
-        self.string02=''
+        self.calculer_bouton = bouton(parent, 0, 2 , "calculer", self.calculer)
+        self.clear_bouton = bouton(parent, 1, 2, "Clear", self.clear)
 
-        # init // widget contenant grids
-        self.contenant = tk.Frame(self.parent)
-        self.contenant.grid(row=x, column=y)
-
-        self.button_width = 7
-        self.button_height = 1
-
-        # init // variables contenant les input
-        self.io01 = ""
-        self.io02 = ""
-        self.io03 = ""
-
-        # widget de saisie
-        self.entry(0,0)
-
-        # widget affichage de l'ouput
-        self.entry_output_cell(3,0)
-
-        # init // boutons
-        self.button_calculer_click("calculer", 0, 2)
-        self.button_clear_click("Clear", 1, 2)
-
-    # ---------- construtor FIN ----------
+        self.saisie_point = saisie(parent, 0, 0, "point")
+        self.saisie_goal = saisie(parent, 1, 0, "goal")
+        self.saisie_respawn = saisie(parent, 2, 0,"Respawn rate")
+    
+        self.output = texte(parent, 3, 0, 50, 3).get_text()
 
 
-
-    # fonction qui gere et affiche les widget de saisie
-    def entry(self, x, y):
-        # 1er widget de saisie
-        self.i01 = tk.Entry(self.contenant, width=10)
-        self.i01.grid(row=x, column=y+1)
-        self.i01_label = tk.Label(self.contenant, text="point")
-        self.i01_label.grid(row=x, column=y)
-        # 2nd widget de saisie
-        self.i02 = tk.Entry(self.contenant, width=10)
-        self.i02.grid(row=x+1, column=y+1)
-        self.i02_label = tk.Label(self.contenant, text="goal")
-        self.i02_label.grid(row=x+1, column=y)
-        # 3eme widget de saisie
-        self.i03 = tk.Entry(self.contenant, width=10)
-        self.i03.grid(row=x+2, column=y+1)
-        self.i03_label = tk.Label(self.contenant, text="time")
-        self.i03_label.grid(row=x+2, column=y)
-
-
-    # fonction qui gere la fenetre d'output
-    def entry_output_cell(self, x, y):
-        self.o = tk.Text(
-            self.contenant, font=10, state=tk.DISABLED,
-            height=3, width=50)
-        self.o.grid(row=x, column=y, columnspan=3)
-
-
-    # fonction réalisant les calculs et affiche les messages
     def calculer(self):
-        # on récupere les inputs et les attribue dans des variables
-        # chaque variables est testée puis typée
-        self.point = self.i01.get()
-        self.point = test_int(self.point)
-        self.goal = self.i02.get()
-        self.goal = test_int(self.goal)
-        self.time = self.i03.get()
-        self.time =test_float(self.time)
-        # calcul
-        progress = test_int(1000000/self.point)
-        time_one_pp = progress*self.time
+        p = test_int(self.saisie_point.get_value())
+        g = test_int(self.saisie_goal.get_value())
+        r = test_float(self.saisie_respawn.get_value())
+        t = r + 0.8
+
+        progress = 1000000/p
+        time_one_pp = progress*t
         time_one_pp = ceil(test_int(time_one_pp))
-        time_goal = self.goal*time_one_pp
+        time_goal = g*time_one_pp
         # génération de valeur temporelles, attribution en variable
         self.time_one_pp = timedelta(seconds=time_one_pp)
         self.time_goal = timedelta(seconds=time_goal)
+
         # a chaque calcul, deux lignes de message sont générées et affichées
         self.string01 = f"Temps passé pour avoir 1 PP: {self.time_one_pp} hh:mm:ss"
-        self.string02 = f"Temps passé pour avoir {self.goal} PP: {self.time_goal} hh:mm:ss"
-        self.display( self.string01 + '\n' + self.string02)
+        self.string02 = f"Temps passé pour avoir {g} PP: {self.time_goal} hh:mm:ss"
+        self.display( self.string01 + '\n' + self.string02 )
 
 
-    # bouton pour activer la fonction calculer
-    def button_calculer_click(self, titre, x, y):
-        self.b = tk.Button(
-            self.contenant, text=titre, command=self.calculer,
-            width=self.button_width, height=self.button_height)
-        self.b.grid(row=x, column=y)
+    def display(self, text):
+        self.output.config(state=tk.NORMAL, font="Calibri")
+        self.output.delete('1.0', tk.END)
+        self.output.insert('1.0', text)
+        self.output.config(state=tk.DISABLED)
 
 
-    # fonction qui enleve les outputs de l'affichage
     def clear(self):
-        self.goal = ""
-        self.time = ""
-        self.point = ""
         self.display('')
 
 
-    # bouton pour activer la fonction clear
-    def button_clear_click(self, titre, x, y):
-        self.b = tk.Button(
-            self.contenant, text=titre, command=self.clear,
-            width=self.button_width, height=self.button_height)
-        self.b.grid(row=x, column=y)
-
-
-    # fonction qui gere l'affichage dans la fentre d'output
-    def display(self, text_):
-        self.o.config(state=tk.NORMAL)
-        self.o.delete('1.0', tk.END)
-        self.o.insert('1.0', text_)
-        self.o.config(state=tk.DISABLED)
 
 
 
+class saisie(tk.Entry):
+    def __init__(self, parent, xRow, yCol, label=None):
+        tk.Entry.__init__(self)
+        self.xRow = xRow
+        self.yCol = yCol
+        self.label = label
+
+        self.s = tk.Entry(parent, width=10)
+        self.s.grid(row=self.xRow, column=self.yCol+1)
+        self.s_label = tk.Label(parent, text=self.label)
+        self.s_label.grid(row=self.xRow, column=self.yCol)
+
+
+    def get_value(self):
+        return self.s.get()
 
 
 
 
-class Appli:
-    def __init__(self, master):
-        self.master = master
 
-        pp_calculator(self.master, 0, 0)
+class texte(tk.Text):
+    def __init__(self, parent, xRow, yCol, w, h):
+        tk.Text.__init__(self)
+        self.xRow = xRow
+        self.yCol = yCol
+        self.width = w
+        self.height = h
+
+        self.boite_texte = tk.Text(parent, width=self.width, height=self.height, state=tk.DISABLED)
+        self.boite_texte.grid(row=self.xRow, column=self.yCol, columnspan=3)
+
+
+    def get_text(self):
+        return self.boite_texte
+
+
+
+
+
+class bouton(tk.Button):
+    def __init__(self, parent, xRow, yCol, titre, cmd):
+        tk.Button.__init__(self)
+        self.bouton = tk.Button(parent, text=titre, height=1, width=7, command=cmd)
+        self.xRow = xRow
+        self.yCol = yCol
+        self.bouton.grid(row=self.xRow, column=self.yCol)
+
+
+
+
+
+class menu_bar(tk.Menu):
+    def __init__(self, parent):
+        tk.Menu.__init__(self)
+
+        menu_Fichier = tk.Menu(self, tearoff=0)
+        menu_Fichier.add_command(label="Nouveau")
+        menu_Fichier.add_command(label="Ouvrir")
+        menu_Fichier.add_command(label="Enregistrer")
+        menu_Fichier.add_command(label="Enregistrer Sous")
+        menu_Fichier.add_command(label="Imprimer")
+        menu_Fichier.add_separator()
+        menu_Fichier.add_command(label="Quitter", command=self.quitter)
+
+        menu_Help = tk.Menu(self, tearoff=0)
+        menu_Help.add_command(label="Documentation")
+        menu_Help.add_command(label="Mail au support")
+
+        self.add_cascade(label="Fichier", menu=menu_Fichier)
+        self.add_cascade(label="Help", menu=menu_Help)
+
+
+    def quitter(self):
+        exit()
+
+
+
+
 
 # ==================================================================================================
 #   SCRIPT
 # ==================================================================================================
 
 root = tk.Tk()
+
+root.config(menu=menu_bar(root))
 root.title("Perk Points Calculator")
 root.iconbitmap("pp_calculator_poo/logo.ico")
-application = Appli(root)
+
+interface(root)
 
 root.mainloop()
