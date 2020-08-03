4e91520a6170ae462944be1ce61caec304b52764
JackOWhiskey <jmarboeufpro@gmail.com>
2020-08-03 18:49:21 +0200

    [update] suppression ngu_idle_calculator

--- ngu_idle_calculator/old/ppCalculetteArgs.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set path2File=script_with_args
-set nbPoint=126098
-set time=2.5
-set goalPP=100
-
-python %path2File%/pp_calculator_with_args.py %nbPoint% %time% %goalPP%
--- ngu_idle_calculator/pp_calculator_gui.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-title salut_ca_va
-
-set path2File=pp_calculator_poo
-
-python %path2File%/guiPoo.py
