b48a65b4314dd77626271151026efe7617706097
JackOWhiskey <jmarboeufpro@gmail.com>
2020-09-18 18:30:52 +0200

    [modif] mineur // login_func.py mise en variable des éléments composants le chemin du log

--- gui_learning/ngu_idle_PP_calculator/pp_calculator_poo/loging_func.py
+++ gui_learning/ngu_idle_PP_calculator/pp_calculator_poo/loging_func.py
@@ -15,7 +15,10 @@ from logging.handlers import RotatingFileHandler
 #   FONCTIONS
 # ==================================================================================================
 
-logging.basicConfig(filename="log/ngu_display_messages.log",
+path_to_log="log/"
+log_file_name="ngu_display_messages.log"
+
+logging.basicConfig(filename=f"{path_to_log}{log_file_name}",
                     format='%(asctime)s :: %(message)s',
                     filemode='a')
 
