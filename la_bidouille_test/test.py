b43c9a61dac5e223af0c6fcd3cee99aa64ce979f
JackOWhiskey <jmarboeufpro@gmail.com>
2020-09-22 21:25:56 +0200

    fatigue

---
+++ la_bidouille_test/test.py
@@ -0,0 +1,8 @@
+
+import random
+
+word = "coucou"
+
+print(word[0])
+
+word[1] = "a"
--- pendu/pendu_main.py
+++ pendu/pendu_main.py
@@ -11,15 +11,17 @@ from tkinter import filedialog
 
 import configparser
 import os
+import random
 
 from words import words_list
+
 
 # ==================================================================================================
 #   VARIABLES GLOBALES
 # ==================================================================================================
 
 player_name = None
-
+save_file = None
 # ==================================================================================================
 #   DICT
 # ==================================================================================================
@@ -92,12 +94,14 @@ def quitter():
 # fonctions liées à l'interface autre que le menu
 def add_player(name, file_save):
     check_empty_file = os.stat(file_save).st_size == 0
+    name = name.upper()
     if check_empty_file:
         with open(file_save, 'a') as fichier:
             fichier.write(f"{name}=10")
+
     if not check_empty_file:
         with open(file_save, 'a') as fichier:
-            fichier.write(f"\n{name}=10")
+                fichier.write(f"\n{name}=10")
 
 
 def get_list_players(file_save):
@@ -120,13 +124,43 @@ def get_score_player_name(name, file_save):
                 return nom_joueur, score
 
 
-def get_liste_mots(fichier):
-    import words
+def write_player_score(name, file_save, score):
+    with open(file_save, 'w') as fichier:
+        lignes = fichier.readlines()
+        nb_lignes = len(lignes)
+        for i in nb_lignes:
+            index = i
+            for l in lignes:
+                nom_joueur = l.split("=")[0]
+                score = l.split("=")[1]
+                if name == nom_joueur:
+                    lignes[index] = f"{nom_joueur}={score}"
+                    fichier.writelines(lignes)
 
 
+def hide_word(word):
+    mot = []
+    mot_long = len(word)
+    for i in range(mot_long):
+        mot.insert(i, "_ ")
+    return mot
 
 
+def half_guessed_word(word, lettre, atempt):
 
+    atempt = atempt
+    mot = []
+    mot_long = len(word)
+    word = list(word)
+    if lettre in word:
+        for i in range(mot_long):
+            if word[i] == lettre:
+                mot.insert(i, lettre)
+            else:
+                mot.insert(i, "_ ")
+    else:
+        atempt -= 1
+    return mot, atempt
 
 
 
@@ -140,74 +174,104 @@ def get_liste_mots(fichier):
 class interface_main(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self)
+        self.parent = parent
         self.add_player_input = None
-        self.player_name = None
-        self.player_score = None
-        self.save_file = None
+
+        self.player = joueur()
 
         self.label_player = tk.Label(parent)
-        self.main_display = display(parent, 0, 0, 20, 10, 20)
-        self.select_player_input = saisie(parent, 1, 2, "nom du joueur")
-        self.select_player = bouton(parent, 1, 1, "Valider Joueur", self.manage_player)
-        self.pendu_jouer = bouton(parent, 2, 1, "jouer", self.pendu_game)
+        self.label_player.grid(row=0, column=1, sticky='nsew')
+        self.main_display = display(parent, 0, 0, 20, 10, 20).get_text()
 
-
-    def check_save_file(self):
-        self.save_file = get_config_element("FILES", "save_file")
-        
-        if not self.save_file:
-            create_save_file()
-            self.save_file = get_config_element("FILES", "save_file")
-
-        if not os.path.exists(self.save_file):
-            create_save_file()
+        self.select_player_input = saisie(parent, 1, 1, "nom du joueur")
+        self.pendu_play_button = bouton(parent, 2, 1, "jouer", self.pendu_game)
 
 
 
     def manage_player(self):
-        self.check_save_file()
-        players_list = get_list_players(self.save_file)
+        players_list = get_list_players(save_file)
         player_name = self.select_player_input.get_value()
-        player_name = player_name.upper()
 
-        if not player_name:
+        if player_name == False:
             player_name = "ANONYME"
-            add_player(player_name, self.save_file)
-            self.player_name = player_name
-            string = f"nom du joueur: {self.player_name} // score: 10"
-            # self.affichage(self.output, string)
-            self.label_player.grid(row=0, column=1, sticky='nsew')
+            if player_name not in players_list:
+                add_player(player_name, save_file)
+                self.player = joueur(player_name, 10)
+                string = str(f"nom du joueur: {self.player.get_name} // score: {self.player.get_score}")
+                self.label_player.config(text=string)
+
+            else:
+                n, s = get_score_player_name(player_name, save_file)
+                self.player.set_name(n)
+                self.player.set_score(s)
+                string = str(f"nom du joueur: {self.player.get_name} // score: {self.player.get_score}")
+                self.label_player.config(text=string)
+
+
+        elif player_name in players_list:
+            n, s = get_score_player_name(player_name, save_file)
+            self.player = joueur(n, s)
+            string = str(f"nom du joueur: {self.player.get_name} // score: {self.player.get_score}")
             self.label_player.config(text=string)
 
-        if player_name not in players_list:
-            add_player(player_name, self.save_file)
-            self.player_name = player_name
-            string = str(f"nom du joueur: {self.player_name} // score: 10")
-            # self.affichage(self.output, string)
-            self.label_player.grid(row=0, column=1, sticky='nsew')
+        elif player_name not in players_list:
+            add_player(player_name, save_file)
+            n, s = get_score_player_name(player_name, save_file)
+            self.player = joueur(n, s)
+            string = str(f"nom du joueur: {self.player.get_name} // score: {self.player.get_score}")
             self.label_player.config(text=string)
-        else:
-            self.player_name, self.player_score = get_score_player_name(player_name, self.save_file)
-            string = str(f"nom du joueur: {self.player_name} // score: {self.player_score}")
-            # self.affichage(self.output, string)
-            self.label_player.grid(row=0, column=1, sticky='nsew')
-            self.label_player.config(text=string)
+
 
 
     def pendu_game(self):
-        self.check_save_file()
-        if not self.player_name:
-            player_name = "ANONYME"
-            add_player(player_name, self.save_file)
-            self.player_name = player_name
-            string = f"nom du joueur: {self.player_name} // score: 10"
-            # self.affichage(self.output, string)
-            self.label_player.grid(row=0, column=4)
+        mot = mot(tentative=10)
+
+
+        if self.select_player_input.get_value() == False:
+            string = "Renseignez un nom de joueur"
             self.label_player.config(text=string)
 
-        mot_pendu = words_list
+        else:
+            player_input = self.select_player_input.get_value()
+            players = get_list_players(save_file)
 
-        self.affichage(self.main_display, mot_pendu)
+            if player_input not in players:
+                add_player(player_input, save_file)
+                self.player.set_name(player_input)
+                self.player.set_score(10)
+
+            else:
+                n, s = get_score_player_name(player_input, save_file)
+                self.player.set_name(n)
+                self.player.set_score(s)
+
+            mot_random = random.choice(words_list)
+            mot.set_name(mot_random)
+
+            self.lettre_input = saisie(self.parent, 3, 2, "lettre")
+            self.lettre_button = bouton(self.parent, 3, 1, "valider lettre", self.check_lettre)
+
+
+    def check_lettre(self):
+        name = self.mot.get_name()
+        tentative = self.mot.get_tentative()
+
+        if tentative != 0:
+            a = self.lettre_input.get_value()
+            name, tentative  = half_guessed_word(name, a, tentative)
+            mot.set_name(name)
+            mot.set_tentative(tentative)
+
+            string = f"mot:{name}  \ntentative:{tentative}"
+            affichage(self.main_display, string)
+
+
+
+
+
+
+
+
 
 
 
@@ -219,19 +283,72 @@ class interface_main(tk.Frame):
             liste: liste des éléments à afficher
 
         '''
-        if type(liste) != list:
-            objet.config(state=tk.NORMAL, font="Calibri")
-            objet.delete('1.0', tk.END)
-            objet.insert('1.0', liste)
-            objet.config(state=tk.DISABLED)
-        else:
+        if type(liste) is list:
             objet.config(state=tk.NORMAL, font="Calibri")
             objet.delete('1.0', tk.END)
             for l in liste:
                 objet.insert('1.0', l)
             objet.config(state=tk.DISABLED)
 
+        else:
+            objet.config(state=tk.NORMAL, font="Calibri")
+            objet.delete('1.0', tk.END)
+            objet.insert('1.0', liste)
+            objet.config(state=tk.DISABLED)
 
+
+
+class lettre():
+    def __init__(self, lettre=None):
+        self.lettre = lettre
+
+    def get_name(self):
+        return self.lettre
+
+
+    def set_name(self, value):
+        self.lettre = value
+
+
+
+
+class mot():
+    def __init__(self, name=None, tentative=None):
+        self.name = name
+        self.tentative = tentative
+
+    def get_name(self):
+        return self.name
+
+    def get_tentative(self):
+        return self.tentative
+
+    def set_name(self, value):
+        self.name = value
+
+    def set_tentative(self, value):
+        self.tentative = value
+
+    def lower_tentative(self, value):
+        self.tentative = self.get_tentative() - value
+
+
+class joueur():
+    def __init__(self, name=None, score=None):
+        self.name = name
+        self.score = score
+
+    def get_name(self):
+        return self.name
+
+    def get_score(self):
+        return self.score
+
+    def set_name(self, value):
+        self.name = value
+
+    def set_score(self, value):
+        self.score = value
 
 
 
@@ -315,16 +432,19 @@ class saisie(tk.Entry):
                             )
         # création du nom de la zone de saisie (label)
         self.saisie_label = tk.Label(parent, text=self.label)
-        self.saisie_label.grid(row=self.xRow, 
-                                column=self.yCol, 
-                                sticky="w"
-                                )
+        self.saisie_label.grid(row=self.xRow, column=self.yCol, sticky="nsew")
+
 
     def get_value(self):
         '''
             Méthode qui récupere la valeur rentrée
         '''
-        return self.saisie.get()
+        a = self.saisie.get()
+        
+        if not a:
+            return False
+        else:
+            return self.saisie.get()
 
 
 
@@ -363,11 +483,12 @@ class menu_bar(tk.Menu):
 # ==================================================================================================
 #   SCRIPT
 # ==================================================================================================
+save_file = get_config_element("FILES", "save_file")
 
 root = tk.Tk()
 root.title("Jeu du pendu")
 root.config(menu=menu_bar(root))
-root.geometry("500x200")
+root.geometry("600x200")
 
 interface_main(root)
 
